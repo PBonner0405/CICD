6697e51bf31954b43effbc175a6135604690cc3e
vadymyak <k100cool@outlook.com>
2023-06-01 15:59:30 -0600

    update cicd

--- actions/ci/script.sh
+++ actions/ci/script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -l
 ### Install PM2
-npm i -g pm2
-pm2 --version
+npm i -g serverless@$SLS_VERSION
+serverless --version
 ### API Deploy ###
 echo "Deploying API Server"
 sls deploy --stage $STAGE --verbose --region $REGION
-pm2 start --name 'Testing Server' index.js
